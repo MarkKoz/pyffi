d4efb2de6f2f08c486cedceafd76519ae5159a87
Mark <1515135+MarkKoz@users.noreply.github.com>
2025-12-31 20:37:32 -0500

    Write bonepriorities test files to temporary directory

--- tests/spells/kf/test_getsetbonepriorities.py
+++ tests/spells/kf/test_getsetbonepriorities.py
@@ -1,68 +1,63 @@
 """Tests for the get/setbonepriorities spells."""
 
-import codecs
-import os.path
-
-import tempfile
-import os
 import shutil
+import importlib.resources
+from pathlib import Path
 
+import tests.spells.kf
 from pyffi.formats.nif import NifFormat
 from tests.scripts.nif import call_niftoaster
-
-from os.path import dirname
-dir_path = __file__
-for i in range(1):  # recurse up to root repo dir
-    dir_path = dirname(dir_path)
-test_root = dir_path
-input_files = os.path.join(test_root, 'spells', 'kf').replace("\\", "/")
 
 class TestGetSetBonePrioritiesOblivion:
 
     file_name = "test_controllersequence.kf"
     txt_name = "test_controllersequence_bonepriorities.txt"
-    kffile = os.path.join(test_root, file_name)
-    kffile2 = os.path.join(test_root, "_" + file_name)
-    txtfile = os.path.join(test_root, txt_name)
 
     @staticmethod
-    def check_priorities(filename, priorities):
+    def check_priorities(path, priorities):
         """helper function to check priorities"""
         data = NifFormat.Data()
-        with open(filename, "rb") as stream:
+        with path.open("rb") as stream:
             data.read(stream)
         assert len(data.roots) == 1 
         seq = data.roots[0]
         assert isinstance(seq, NifFormat.NiControllerSequence)
         assert [block.priority for block in seq.controlled_blocks] == priorities
 
-    def test_check_get_set_bonepriorities(self):
-        TestGetSetBonePrioritiesOblivion.check_priorities(self.kffile, [27, 27, 75])
-        toaster = call_niftoaster("--raise", "modify_getbonepriorities", self.kffile)
-        assert list(toaster.files_done) == [self.kffile]
-        assert os.path.exists(self.txtfile)
-        with codecs.open(self.txtfile, "rb", encoding="ascii") as stream:
-            contents = stream.read()
-            assert contents == '[TestAction]\r\nBip01=27\r\nBip01 Pelvis=27\r\nBip01 Spine=75\r\n'
-        with codecs.open(self.txtfile, "wb", encoding="ascii") as stream:
-            stream.write("[TestAction]\n")
-            stream.write("Bip01=33\n")
-            stream.write("Bip01 Pelvis=29\n")
-            stream.write("Bip01 Spine=42\n") # .replace('\r\n', '\n')) # replace probably not needed; just in case
-        toaster = call_niftoaster("--raise", "modify_setbonepriorities", "--prefix=_", self.kffile)
-        assert list(toaster.files_done) == [self.kffile]
-        self.check_priorities(self.kffile2, [33, 29, 42])
+    def test_check_get_set_bonepriorities(self, tmp_path):
+        kf_file = tmp_path / self.file_name
+        prefixed_kf_file = tmp_path / ("_" + self.file_name)
+        txt_file = tmp_path / self.txt_name
+
+        with importlib.resources.path(tests.spells.kf, self.file_name) as original_kf_file:
+            shutil.copy(original_kf_file, kf_file)
+
+        self.check_priorities(kf_file, [27, 27, 75])
+        toaster = call_niftoaster("--raise", "modify_getbonepriorities", str(kf_file))
+
+        assert list(map(Path, toaster.files_done)) == [kf_file]
+        assert txt_file.read_bytes() == b'[TestAction]\r\nBip01=27\r\nBip01 Pelvis=27\r\nBip01 Spine=75\r\n'
+
+        with txt_file.open("wb") as stream:
+            stream.write(b"[TestAction]\n")
+            stream.write(b"Bip01=33\n")
+            stream.write(b"Bip01 Pelvis=29\n")
+            stream.write(b"Bip01 Spine=42\n")
+        toaster = call_niftoaster("--raise", "modify_setbonepriorities", "--prefix=_", str(kf_file))
+
+        assert list(map(Path, toaster.files_done)) == [kf_file]
+        self.check_priorities(prefixed_kf_file, [33, 29, 42])
+
         # test crlf write
-        with codecs.open(self.txtfile, "wb", encoding="ascii") as stream:
-            stream.write("[TestAction]\n")
-            stream.write("Bip01=38\n")
-            stream.write("Bip01 Pelvis=22\n")
-            stream.write("Bip01 Spine=47\n")
-        toaster = call_niftoaster("--raise", "modify_setbonepriorities", "--prefix=_", self.kffile)
-        assert list(toaster.files_done) == [self.kffile]
-        self.check_priorities(self.kffile2, [38, 22, 47])
-        os.remove(self.txtfile)
-        os.remove(self.kffile2)
+        with txt_file.open("wb") as stream:
+            stream.write(b"[TestAction]\r\n")
+            stream.write(b"Bip01=38\r\n")
+            stream.write(b"Bip01 Pelvis=22\r\n")
+            stream.write(b"Bip01 Spine=47\r\n")
+        toaster = call_niftoaster("--raise", "modify_setbonepriorities", "--prefix=_", str(kf_file))
+
+        assert list(map(Path, toaster.files_done)) == [kf_file]
+        self.check_priorities(prefixed_kf_file, [38, 22, 47])
 
 
 class TestGetSetBonePrioritiesFallout3(TestGetSetBonePrioritiesOblivion):
